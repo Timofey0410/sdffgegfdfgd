28441867ce5e84068e2cf2ec6fe633509fc0a682
Timofey0410 <131515731+Timofey0410@users.noreply.github.com>
2023-05-27 11:19:04 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("куда пойдём")
+way = input()
+if way == "left":
+    print("Идём на лево")
+elif way == "right":
+    print("идём направо")
+elif way == "straight":
+    print("идём прямо")
+else:
+    print("Не знаю куда идти")
